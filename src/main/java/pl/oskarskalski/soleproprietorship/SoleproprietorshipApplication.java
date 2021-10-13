f99c507909f0969089abc0827619f8624b5bf46f
oskarskalski <os.sk.gr@gmail.com>
2021-10-13 23:24:11 +0200

    Init project commit

---
+++ src/main/java/pl/oskarskalski/soleproprietorship/SoleproprietorshipApplication.java
@@ -0,0 +1,13 @@
+package pl.oskarskalski.soleproprietorship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleproprietorshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoleproprietorshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/oskarskalski/soleproprietorship/SoleproprietorshipApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.oskarskalski.soleproprietorship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleproprietorshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
