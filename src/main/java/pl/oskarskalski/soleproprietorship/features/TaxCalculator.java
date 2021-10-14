6a432bfadb3909916ed4c50f399babca118f5659
oskarskalski <os.sk.gr@gmail.com>
2021-10-14 15:39:54 +0200

    Fixed rounding in tax calculation

--- src/main/java/pl/oskarskalski/soleproprietorship/features/TaxCalculator.java
+++ src/main/java/pl/oskarskalski/soleproprietorship/features/TaxCalculator.java
@@ -1,13 +1,14 @@
 package pl.oskarskalski.soleproprietorship.features;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class TaxCalculator {
     public double calculateTaxAmount(double currentTaxPercent, double revenue) {
         BigDecimal revenueAmountAsBD = new BigDecimal(revenue);
         BigDecimal taxPercentAsBG = new BigDecimal(currentTaxPercent);
         BigDecimal result = revenueAmountAsBD.multiply(taxPercentAsBG);
-        return result.doubleValue();
+        return result.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 
 //    See NetRevenueCalculator.java file to see some explanation about this code
